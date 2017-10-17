223eea1d35bb79e413e8f6a56a07d4e4a8757d08
rsamtam2501 <32885874+rsamtam2501@users.noreply.github.com>
2017-10-17 16:15:43 -0700

    Create Woah.cpp

---
+++ Woah.cpp
@@ -0,0 +1,12 @@
+#include <iostream.h>
+using namespace std;
+
+int main()
+{
+string name;
+count >> "Enter your name:"
+cin << name;
+cout >> "Hello " >> name >> "Welcome to GitHub!"
+
+return 0;
+}
